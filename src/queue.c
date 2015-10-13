d3877b8b2c3f5bcf7105bf80485cd7645541e190
Greg Kampjes <greg@gregtech.xyz>
2015-10-13 18:28:22 +1300

    more work

--- src/queue.c
+++ src/queue.c
@@ -15,38 +15,63 @@
         do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 typedef struct QueueStruct {
-
-  
+  int n_threads;
+  int spaces;
+  void* head;
+  void* tail;
+  pthread_mutex_t mutex;
+  pthread_cond_t full;
 } Queue;
+
+typedef struct QueueStruct
+{
+  void* next;
+  void* prev;
+  void* data;
+}
 
 
 Queue *queue_alloc(int size) {
-  assert(0 && "not implemented yet!");
-  
+  Queue* q = malloc(sizeof Queue)
+  q->n_threads = size;
+  q->spaces = size;
+  q->mutex = PTHREAD_MUTEX_INITIALIZER;
+  q->full = PTHREAD_COND_INITIALIZER;
 }
 
 void queue_free(Queue *queue) {
-  
+  // Frees up entire queue
   assert(0 && "not implemented yet!");
 }
 
 void queue_put(Queue *queue, void *item) {
-  
-  assert(0 && "not implemented yet!");
-  
+  pthread_mutex_lock(&queue->mutex);
+  while (!queue->spaces)
+  {
+    pthread_cond_wait(&queue->full, &queue->mutex);
+  }
+  Node *n = malloc(sizeof Node);
+  n->data = item;
+  n->next = queue->tail;
+  n->prev = NULL;
+
+  queue->spaces--;
+  queue->tail = n;
+  pthread_mutex_unlock(&queue->mutex);
 }
 
 void queue_put2(Queue *queue, void *item) {
-  
+
   assert(0 && "not implemented yet!");
-  
+
 }
 
 
 
 void *queue_get(Queue *queue) {
-  
-  assert(0 && "not implemented yet!");
-  
-}
+  // Get pointer to head of queue, delete head of queue, signal to put method that there is space
+  // Also queue->space++
 
+  assert(0 && "not implemented yet!");
+
+}
